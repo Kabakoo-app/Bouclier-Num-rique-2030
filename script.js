d878ed2186126e93b5fc8af6c92dd50bf8a5dd00
djiredjibril <djibrilpendadjire@gmail.com>
2024-06-26 22:45:56 +0000

    Copyrighting en cours

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const loader = document.querySelector(".loader")
     const loading = document.querySelector(".loading")
     const canvas = document.getElementById('drawingCanvas');
-    const generateImage  = document.querySelector('.generateImage');
-    const restartButton  = document.querySelector('.restart');
-    const imageGenerate  = document.querySelector('.imageGenerate');
+    const generateImage = document.querySelector('.generateImage');
+    const restartButton = document.querySelector('.restart');
+    const imageGenerate = document.querySelector('.imageGenerate');
 
     const home = document.querySelector(".home");
     const header = document.querySelector(".header");
     const button = document.querySelector(".button");
-    
-    canvas.width = windowWidth / 1.5 ;
+
+    canvas.width = windowWidth / 1.5;
     canvas.height = windowHeight - 200
 
     const context = canvas.getContext('2d');
@@ -117,39 +117,39 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     function fetchDataFromBothAPIs(sketch, description) {
-    const fetch1 = fetch(`${API_URL}/media/enhance_sketch/`,  {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ sketch, description })
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok for API 1: ' + response.statusText);
-            }
-            return response.json();
-        });
-
-    const fetch2 = fetch(`${API_URL}/media/get_enhanced_sketch?user_uid=60d8e13d-d318-4f9c-b077-5e2e68ecf4aa`,  {
-        method: 'GET'
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok for API 2: ' + response.statusText);
-            }
-            return response.json();
-        });
-
-    return Promise.all([fetch1, fetch2])
-        .then(results => {
-            const [data1, data2] = results;
-            return { data1, data2 };
+        const fetch1 = fetch(`${API_URL}/media/enhance_sketch/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ sketch, description })
         })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-}
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok for API 1: ' + response.statusText);
+                }
+                return response.json();
+            });
+
+        const fetch2 = fetch(`${API_URL}/media/get_enhanced_sketch?user_uid=60d8e13d-d318-4f9c-b077-5e2e68ecf4aa`, {
+            method: 'GET'
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok for API 2: ' + response.statusText);
+                }
+                return response.json();
+            });
+
+        return Promise.all([fetch1, fetch2])
+            .then(results => {
+                const [data1, data2] = results;
+                return { data1, data2 };
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    }
 
     window.setShape = function (newShape, button) {
         shape = newShape;
@@ -158,10 +158,10 @@ document.addEventListener("DOMContentLoaded", function () {
         button.classList.add('active');
     };
 
-    window.startCreation = function() {
+    window.startCreation = function () {
         document.querySelector(".home").style.display = "none";
         document.querySelector(".header").style.display = "none";
-        document.querySelector(".button").style.display = "none";    
+        document.querySelector(".button").style.display = "none";
         document.querySelector(".creation-area").style.marginTop = "0";
         document.getElementById("creationArea").style.display = "flex";
         document.getElementById("nameDescription").style.display = "flex";
@@ -172,22 +172,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     window.submitCreation = async function () {
-        const  description= document.getElementById('mainFunction').value;
-        loader.style.display = "flex";
-        loading.style.display = "block"
+        const description = document.getElementById('mainFunction').value;
         const sketch = canvas.toDataURL('image/png');
 
+
+        if (!description.length) {
+            return alert("Tu dois choisir la fonction principale de ton objet avant de pouvoir continuer.")
+        }
+        loader.style.display = "flex";
+        loading.style.display = "block"
         fetchDataFromBothAPIs(sketch, description)
 
-        .then(({ data1, data2 }) => {
-            const { data : { enhanced_sketch_uri }} = data2
-            loading.style.display = "none"
-            generateImage.style.display = 'block';
-            imageGenerate.src = `https://s3.us-east-2.amazonaws.com/files.kabakoo.africa/${enhanced_sketch_uri}`
-            restartButton.style.display = 'block';
-            // Do something with data1 and data2
-        });
-     
+            .then(({ data1, data2 }) => {
+                const { data: { enhanced_sketch_uri } } = data2
+                loading.style.display = "none"
+                generateImage.style.display = 'block';
+                imageGenerate.src = `https://s3.us-east-2.amazonaws.com/files.kabakoo.africa/${enhanced_sketch_uri}`
+                restartButton.style.display = 'block';
+                // Do something with data1 and data2
+            });
+
         // generateQRCode(data.qrCodeData);
     };
 
